9bc0c4ddb57cac7804e33af01fc2beaf528895d0
Shilan <shma8897@uni.sydney.edu.au>
2022-06-24 12:36:15 +1000

    Basic structure

---
+++ src/About.js
@@ -0,0 +1,35 @@
+import './About.css';
+const EducationsData = [
+    {id: 1, uni: "UNSW", degree: "Bachelor of Information System"},
+    {id: 2, uni: "USYD", degree: "Master of Information Technology, Major in Software Engineering"}
+]
+const educationList = EducationsData.map((education) => 
+    <li>{education.uni} : {education.degree}</li>
+);
+
+const SESkillData = [
+    {id: 1, name: "Java", level: 3},
+    {id: 2, name: "Python", level: 3},
+    {id: 3, name: "Web", level: 4}
+]
+const SESkillList = SESkillData.map((skill) => 
+    <li>{skill.name}  -  {skill.level}</li>
+);
+function About() {
+  return (
+    <div className="About">
+      <header className="About-header">
+          Shilan Ma (Software Engineer)
+      </header>
+      <div className="About-info">
+        Education:
+        <ul>{educationList}</ul>
+        <br></br>
+        Skill List:
+        <ul>{SESkillList}</ul>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/LearningBlogs.js
@@ -0,0 +1,23 @@
+import './LearningBlogs.css';
+
+function Blog() {
+    return (
+        <div className="Blog">
+            <header className="Blog-header">
+                Learning Blogs
+          </header>
+        </div>
+    );
+}
+
+function LearningBlogs() {
+  return (
+    <div className="LearningBlogs">
+        <header className="LearningBlogs-header">
+            Learning Blogs
+        </header>
+    </div>
+  );
+}
+
+export default LearningBlogs;
